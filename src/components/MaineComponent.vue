c34d997f939de164fa02f89212606f124bcdaa2a
NicoleWrobel <nicole.wrobel2000@gmail.com>
2022-07-05 17:26:43 +0200

    Initial commit

---
+++ src/components/MaineComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>WeatherApp</h1>
+  <input type="text" placeholder="wpisz lokalizacje" v-model="localeInfo" />
+</template>
+
+<script>
+export default {
+  // domysle dane deklarowane na poczatku
+  data() {
+    return {
+      localeInfo: 'sam',
+    };
+  },
+  // mounted - funkcja wywołująca się po " zamontowaniu komponentu"
+  mounted() {
+    console.log('hello');
+    console.log('');
+    this.localeInfo = 'San Escobar';
+  },
+};
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
