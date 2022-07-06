6b6c64c10db8c36c7271f03347cf1524fb46a016
NicoleWrobel <nicole.wrobel2000@gmail.com>
2022-07-06 16:16:38 +0200

    1 file modified, 2 added

--- src/components/MaineComponent.vue
+++ src/components/MaineComponent.vue
@@ -1,26 +1,65 @@
 <template>
   <h1>WeatherApp</h1>
   <input type="text" placeholder="Wpisz lokalizację" v-model="localeInfo" />
+  <button @click="getLatLon">szukaj</button>
+  <CityDetails :values="cityValues" />
+
+  <div class="weatherDays">
+    <SingleWeather v-for="single of weatherList" :values="single" />
+  </div>
+
 </template>
 
 <script>
+import CityDetails from './CityDetails.vue';
+import SingleWeather from './SingleWeather.vue';
 export default {
+  components: {
+    CityDetails,
+    SingleWeather
+  },
   // "domyślne" dane - te ładowane na początku
   data() {
     return {
       localeInfo: 'Chicago', // domyślna lokalizacja
+      cityValues: {},
+      weatherList: [],
     };
   },
-  // "sprawdzaj / patrz" element, którego nazwa jest nazwą funkji
-  watch: {
-    // patrz na zmienną localInfo
-    localeInfo(newVal) {
-      console.log('newData: ', newVal);
+  // obiekt przetrzymujący wszystkie funkcje dla komponentu
+  methods: {
+    getLatLon() {
+      this.cityValues = {};
+      fetch(
+        'https://api.openweathermap.org/geo/1.0/direct?q=' +
+          this.localeInfo +
+          '&appid=04d03c358e8933ac6823da54c340c97b'
+      )
+        .then((dt) => dt.json())
+        .then((dt) => {
+          this.getWeather(dt[0].lat, dt[0].lon);
+        });
+    },
+    getWeather(lat, lon) {
+      fetch(
+        'https://api.openweathermap.org/data/2.5/forecast?lat=' +
+          lat +
+          '&lon=' +
+          lon +
+          '&appid=04d03c358e8933ac6823da54c340c97b'
+      )
+        .then((dt) => dt.json())
+        .then((dt) => {
+          this.cityValues = dt.city;
+          this.weatherList = dt.list;
+          console.log(dt);
+        });
     },
   },
   // mouted - funkcja wywołująca się po "zamontowaniu komponentu"
   mounted() {
     // console.log('hello');
+    this.getLatLon();
   },
 };
 </script>
@@ -28,5 +67,6 @@ export default {
 <style>
 h1 {
   color: red;
+  margin: 0;
 }
 </style>
---
+++ src/components/SingleWeather.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="single">
+    KOMPONENT POGODOWY <br />
+    {{ values }}
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    values: Object,
+  },
+};
+</script>
+
+<style>
+.single {
+  border: 2px dashed red;
+}
+</style>
