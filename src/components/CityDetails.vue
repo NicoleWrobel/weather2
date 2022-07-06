2da12eb3a7c385b2956d1a081198f5abdbf834ef
NicoleWrobel <nicole.wrobel2000@gmail.com>
2022-07-06 20:00:10 +0200

    3 files modified

--- src/components/CityDetails.vue
+++ src/components/CityDetails.vue
@@ -1,24 +1,50 @@
 <template>
+  <!-- v-if - warunek, jeśli prawdziwy, to wykona to co pomiędzy tagami ( loading ) -->
+  <!-- Object.keys zwraca tablicę z kluczami obiektu np Object.keys({ name : "John" }) => ['name']  -->
+  <!-- .lenght zwraca długość tablicy, gdzie 0 to pusta tablica -->
   <div v-if="Object.keys(values).length == 0" class="loading">loading...</div>
-  <div v-else>
-    <h2>{{ values.name }}</h2>
-    <img
-      :src="'https://countryflagsapi.com/svg/' + values.country"
-      alt="COUNTRY FLAG"
-    />
+  <!-- v-else - kod w środku wykona się jeśli powyższy warunek jest fałszywy -->
+  <div v-else class="cityDetail">
+    <header>
+      <!-- pobieramy dane z values -->
+      <h2>{{ values.name }}</h2>
+      <!-- jeśli chcesz połączyć string z zmienną js użyj + oraz " " -->
+      <!-- dwukropek przed atrybutem zmienia jego wartość na javascriptową -->
+      <img
+        :src="'https://countryflagsapi.com/svg/' + values.country"
+        alt="COUNTRY FLAG"
+      />
+    </header>
     <div class="suntime">
-      <div class="sunrise">{{ unixToDateTime(values.sunrise) }}</div>
-      <div class="sunset">{{ unixToDateTime(values.sunset) }}</div>
+      <div class="sunrise">
+        <span> Wschód słońca </span>
+        <span>
+          <!-- wykorzystujemy funckję napisaną w METHODS -->
+          {{ unixToDateTime(values.sunrise) }}
+        </span>
+      </div>
+      <div class="sunset">
+        <span>Zachód słońca</span>
+        <span>
+          <!-- wykorzystujemy funckję napisaną w METHODS -->
+          {{ unixToDateTime(values.sunset) }}
+        </span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+// ten komponent eksportuje ten object:
 export default {
+  // properties - właściwości ( które dostajemy od rodzica )
   props: {
+    // "cityValues" przekazany od rodzica
     values: Object,
   },
+  // przechowuje wszystkie wykorzystywane w komponencie funcje
   methods: {
+    // funckja zmieniająca unixową datę na "ludzką" H:i:s
     unixToDateTime(unix_timestamp) {
       const date = new Date(unix_timestamp * 1000);
       const hours = date.getHours();
@@ -30,8 +56,37 @@ export default {
 };
 </script>
 
-<style>
-img {
-  max-width: 30vw;
+<style lang="scss">
+.cityDetail {
+  border-radius: 1rem;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+  header {
+    background-color: #213547;
+    color: #fff;
+    border-radius: 1rem 1rem 0 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.25rem;
+    h2 {
+      margin: 0;
+    }
+    img {
+      height: 2rem;
+      margin-left: 1rem;
+    }
+  }
+  .suntime {
+    display: flex;
+    justify-content: space-between;
+    padding: 0.4rem 1rem 1rem 1rem;
+    .sunset,
+    .sunrise {
+      display: flex;
+      flex-direction: column;
+    }
+  }
 }
 </style>
--- src/components/SingleWeather.vue
+++ src/components/SingleWeather.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="single">
+    <!-- pobieramy info z obiektu values -->
+    <!-- {{ values }} -->
     <h3>{{ values.dt_txt }}</h3>
     <div class="temp">
       <ul>
@@ -8,6 +10,8 @@
         <li>max: {{ values.main.temp_max }} &deg; C</li>
         <li>ciśnienie : {{ values.main.pressure }} hPa</li>
       </ul>
+      <!-- jeśli chcesz połączyć string z zmienną js użyj + oraz " " -->
+      <!-- dwukropek przed atrybutem zmienia jego wartość na javascriptową -->
       <img
         :src="
           'http://openweathermap.org/img/wn/' +
@@ -20,15 +24,45 @@
   </div>
 </template>
 <script>
+// ten komponent eksportuje ten object:
 export default {
+  // properties - właściwości ( które dostajemy od rodzica )
   props: {
+    // "single" przekazany od rodzica
     values: Object,
   },
 };
 </script>
 
-<style>
+<style lang="scss">
 .single {
-  border: 2px dashed red;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+  margin: 1rem;
+  border-radius: 1rem;
+  h3 {
+    border-radius: 1rem 1rem 0 0;
+    padding: 0.25rem;
+    color: #fff;
+    background-color: #213547;
+    margin: 0;
+  }
+  .temp {
+    padding: 0.5rem 0 0 1rem;
+    display: flex;
+    justify-content: space-between;
+    ul {
+      padding: 0;
+      margin: 0;
+      list-style: none;
+      li {
+        text-align: left;
+      }
+    }
+    img {
+      aspect-radio: 1/1;
+    }
+  }
 }
 </style>
